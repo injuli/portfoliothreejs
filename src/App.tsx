c5fa458f142a1dc42f7b7a444add7dd4aba66046
injuli <ingrijrm94@gmail.com>
2023-12-16 18:35:55 -0500

    feat(donet): added test example

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import {Canvas} from "@react-three/fiber";
+import {OrbitControls} from "@react-three/drei";
 
 function App() {
-  const [count, setCount] = useState(0)
+  // const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+// tailwind css usage
+    <div className={' bg-indigo-800 absolute w-full h-screen p-0 top-0 left-0'}> 
+      <div className={'h-5/6 p-0 '}>
+        <Canvas camera = {{
+          position: [0,0,7],
+          fov:30,
+
+        }}>
+          <color attach="background" args={["#ececec"]}/>
+// three-drei usage
+          <OrbitControls/> 
+          <mesh rotation ={[Math.PI / 10,10,10]}>
+// three-fiber usage
+            <torusGeometry /> 
+            <meshNormalMaterial/>
+          </mesh>
+        </Canvas>
+        <div className={"text-current text-7xl italic font-light z-40 p-5"}>yaee! you made it </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
   )
 }
 
